4e876dbc8be17c5f07af8f004c6253f80a0fb24e
ehennestad <ehennestad@gmail.com>
2025-04-11 10:28:28 +0200

    Rename plotRelationShips to plotModuleGraph

--- code/+om/+plot/plotRelationships.m
+++
@@ -1,9 +0,0 @@
-function plotRelationships(modelName)
-
-    arguments
-        modelName (1,1) om.enum.Models = "core"
-    end
-    
-    G = om.internal.graph.generateGraph(lower(char(modelName)));
-    om.internal.graphics.InteractiveOpenMINDSPlot(G)
-end
